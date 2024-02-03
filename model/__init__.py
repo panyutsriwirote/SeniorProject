221a141e17f6d3d87efbb9cd6851a8ae5f993a1a
Panyut Sriwirote <mildpanyut@gmail.com>
2024-02-03 23:32:18 +0700

    modify model's __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,10 @@
 from .transition_based import TransitionBasedModel
 from .graph_based import GraphBasedModel
+from .pos_tagger import (
+    POSTagger,
+    train_pos_tagger,
+    tag_treebank
+)
 from .utils import (
     get_train_dev_test,
     train_model,
